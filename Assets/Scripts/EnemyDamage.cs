b61bbad91766dbf47cbc4cd7778eed29879c7fcb
gerin-puig <69816174+gerin-puig@users.noreply.github.com>
2020-08-22 03:15:40 -0400

    turrets + enemy
    turrets now follow and shoot when enemy is within range

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] int hitPoints = 10;
+    [SerializeField] float flashTime = 1f;
+    Color originalColour = Color.red;
+    new MeshRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponentInChildren<MeshRenderer>();
+        originalColour = renderer.material.color;
+        //Debug.Log(originalColour);
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        DamageIndicator();
+        ProcessHit();
+
+    }
+
+    private void DamageIndicator()
+    {
+        renderer.material.color = Color.red;
+        Invoke("ResetColour", flashTime);
+    }
+
+    //in an invoke call
+    private void ResetColour()
+    {
+        renderer.material.color = originalColour;
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints -= 1;
+
+        if (hitPoints < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,9 +6,35 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] ParticleSystem bullet;
+    [SerializeField] float towerRange = 2f;
+
+    
+    private void Start()
+    {
+        var em = bullet.emission;
+        em.enabled = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetEnemy);
+        var em = bullet.emission;
+        if (Vector3.Distance(targetEnemy.transform.position,gameObject.transform.position) < towerRange)
+        {
+            objectToPan.LookAt(targetEnemy);
+            em.enabled = true;
+        }
+        else
+        {
+            em.enabled = false;
+        }
+        
     }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawSphere(transform.position, towerRange);
+    //}
 }
